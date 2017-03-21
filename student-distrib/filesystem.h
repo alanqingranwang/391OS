8221d7d2567ab44144adfaea9f98a8b4df13b664
Jerry Chen <Jerry Chen>
2017-03-21 00:52:37 -0500

    added comments

--- student-distrib/filesystem.h
+++ student-distrib/filesystem.h
@@ -1,5 +1,5 @@
 /* JC
- * filesystem.h - Contains the file descriptor table.
+ * filesystem.h - Functionality declaration for the file system
  * tab size = 3, no space
  */
 
@@ -8,25 +8,31 @@
 
 #include "lib.h"
 
-
-/* https://web.cs.wpi.edu/~rek/DCS/D04/UnixFileSystems.html
- * We need the following structs:
- *		boot block - located in the first few sectors of a file system.
- *			The boot block contains the initial bootstrap program used
- *			to load the operating system.
- *			Typically the first sector contains a boostrap program that
- *			reads in a larger bootstrap program from the next few sectors,
- *			and so forth.
- *		super block - probably not
- * 	inode
- *		data blocks
+/* The file system is already created for us in the file system image.
+ * 	We do not need to create the structure itself.
  */
 
+/* We need to create, 3 functions, plus some initialization to set
+ * up everything. You'll also need to define file_o/c/r/w and dir_o/c/r/w, but
+ *	these will be dummy functions for checkpoint 2.
+ */
 
-/* Create a dentry_t struct */
+/* We need to create a process control block whenever a process is started.
+ * 
+ *
+ */
 
+// need to figure out how to access the file system's boot block, and the file system in general
 
-
+/*  */
+// need a file system init here
+/* should automatically open stdin and stdout, which correspond to the file
+ *		descriptors 0 and 1, respectively. stdin is a read-only file which corresponds
+ *		to keyboard input. stdout is a write-only location in the file array, and marking
+ *		the files as in-use. For the remaining six file descriptors avaialable, an entry
+ *		in the file array is dynamically associated with the file being open'd whenever
+ *		the open system call is made (return -1 if the array is full).
+ */
 
 
 /* The three routines provided by the file system module return -1 on failure
