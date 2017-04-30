c7f973410f245730fe308f6a24d30eff21261496
Wang <qwang78@illinois.edu>
2017-04-30 02:47:55 -0500

    unsafe

--- student-distrib/sched.c
+++ student-distrib/sched.c
@@ -57,77 +57,74 @@ void pit_init()
  */
 void pit_handler()
 {
-	// int32_t p_id = current_process[sched_proc];
+	/* store esp and ebp for old process */
+	asm volatile(
+		"movl %%esp, %0 \n"
+		: "=r" (process_array[current_process[sched_proc]]->return_esp)
+	);
+	asm volatile(
+		"movl %%ebp, %0 \n"
+		: "=r" (process_array[current_process[sched_proc]]->return_ebp)
+	);
 
-	// if (p_id > 7){
-	// 	send_eoi(PIT_IRQ);
-	// 	return;
-	// }
+	int32_t p_id = current_process[sched_proc];
 
-	// if(init_flag){
-	// 	init_flag = 0;
-	// 	in_use[curr_terminal] = 0;
-	// 	send_eoi(PIT_IRQ);
-	// 	execute((uint8_t*)"shell");
-	// }
+	/* Check process id validity */
+	if (p_id > 7){
+		send_eoi(PIT_IRQ);
+		return;
+	}
 
-	// /* store esp and ebp for old process */
-	// asm volatile(
-	// 	"movl %%esp, %0 \n"
-	// 	: "=r" (proc_ESP[p_id])
-	// );
-	// asm volatile(
-	// 	"movl %%ebp, %0 \n"
-	// 	: "=r" (proc_EBP[p_id])
-	// );
+	/* Check if current scheduled process exists.
+	   If not, mark as not in use and execute base shell */
+	if(current_process[sched_proc] == -1){
+		in_use[sched_proc] = 0;
+		send_eoi(PIT_IRQ);
+		execute((uint8_t*)"shell");
+	}
 
-	// /* get new process */
-	// sched_proc = (sched_proc+1)%3;
-	// p_id = current_process[sched_proc];
+	/* get new process */
+	sched_proc = (sched_proc+1)%3;
+	p_id = current_process[sched_proc];
 
-	// if (p_id > 7){
-	// 	send_eoi(PIT_IRQ);
-	// 	return;
-	// }
+	/* Check process id validity */
+	if (p_id > 7){
+		send_eoi(PIT_IRQ);
+		return;
+	}
 
-	// if(p_id < 0 && (curr_terminal == sched_proc)){
-	// 	in_use[curr_terminal] = 0;
-	// 	send_eoi(PIT_IRQ);
-	// 	execute((uint8_t*)"shell");
-	// }
-	// else{
-	// 	sched_proc = curr_terminal;
-	// 	send_eoi(PIT_IRQ);
-	// 	return;
-	// }
+	if(p_id < 0 && (curr_terminal == sched_proc)){
+		in_use[curr_terminal] = 0;
+		send_eoi(PIT_IRQ);
+		execute((uint8_t*)"shell");
+	}
+	else if(p_id < 0){
+		sched_proc = curr_terminal;
+		send_eoi(PIT_IRQ);
+		return;
+	}
 
-	// /* read file into memory */
-	// uint8_t *file_name = process_array[p_id]->comm;
-	// dentry_t dentry;
-	// uint32_t address = PROGRAM_START;
-	// read_dentry_by_name((uint8_t*) file_name, &dentry);
-	// read_data(dentry.inode_idx, 0, (uint8_t *)address, inodes[dentry.inode_idx].file_size);
+	/* set up paging */
+	add_process(current_process[sched_proc]);
+	map_virt_to_phys((uint32_t)VIRT_VID_TERM1 + sched_proc*(0x1000), USER_VIDEO_);
 
-	// /* set up paging */
-	// add_process(p_id);
-
- //   /* prepare tss for context switch */
-	// tss.esp0 = K_STACK_BOTTOM - PROCESS_SIZE * (p_id) - BYTE_SIZE/2;
- // 	tss.ss0 = KERNEL_DS;
+   /* prepare tss for context switch */
+	tss.esp0 = K_STACK_BOTTOM - PROCESS_SIZE * (current_process[sched_proc]) - BYTE_SIZE/2;
+ 	tss.ss0 = KERNEL_DS;
 
 	send_eoi(PIT_IRQ); // wait till the switch is done before continuing work
 
-	// /* restore esp and ebp for return */
-	// asm volatile(
-	// 	"movl %0, %%esp \n"
-	// 	:
-	// 	: "r"(proc_ESP[p_id])
-	// );
-	// asm volatile(
-	// 	"movl %0, %%ebp \n"
-	// 	:
-	// 	: "r"(proc_EBP[p_id])
-	// );
+	/* restore esp and ebp for return */
+	asm volatile(
+		"movl %0, %%esp \n"
+		:
+		: "r"(process_array[current_process[sched_proc]]->return_esp)
+	);
+	asm volatile(
+		"movl %0, %%ebp \n"
+		:
+		: "r"(process_array[current_process[sched_proc]]->return_ebp)
+	);
 }
 
 
